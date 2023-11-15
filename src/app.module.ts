2060c6b5624962d3f1f77a32384a802fde65f56c
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2023-11-15 10:00:10 +0300

    создал контроллеры

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthControllerController } from './auth-controller/auth-controller.controller';
+import { UserControllerController } from './user-controller/user-controller.controller';
+import { FileControllerController } from './file-controller/file-controller.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthControllerController, UserControllerController, FileControllerController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/file-controller/file-controller.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('file-controller')
+export class FileControllerController {}
